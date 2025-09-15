c4c243e3ba090e2eb56e96347b6cc083ae4e5856
RayyanKhan4004 <rk4004104@gmail.com>
2025-09-15 13:21:00 +0500

    removed checks for base64

--- src/controllers/blogController.ts
+++ src/controllers/blogController.ts
@@ -1,41 +1,41 @@
 import { Request, Response, NextFunction } from 'express';
 import { Blog } from '../models/Blog';
 
-function validateBase64Image(image: unknown): asserts image is string {
-    if (typeof image !== 'string') {
-        throw new Error('Image must be a base64 string');
-    }
-    if (!isBase64Image(image)) {
-        throw new Error('Invalid base64 image format');
-    }
-}
+// function validateBase64Image(image: unknown): asserts image is string {
+//     if (typeof image !== 'string') {
+//         throw new Error('Image must be a base64 string');
+//     }
+//     if (!isBase64Image(image)) {
+//         throw new Error('Invalid base64 image format');
+//     }
+// }
 
-function validateBase64Images(images: unknown): asserts images is string[] {
-    if (!Array.isArray(images)) {
-        throw new Error('images must be an array');
-    }
-    if (images.length > 5) {
-        throw new Error('images must contain at most 5 items');
-    }
-    images.forEach((img, index) => {
-        if (!isBase64Image(img)) {
-            throw new Error(`Invalid base64 image format at index ${index}`);
-        }
-    });
-}
+// function validateBase64Images(images: unknown): asserts images is string[] {
+//     if (!Array.isArray(images)) {
+//         throw new Error('images must be an array');
+//     }
+//     if (images.length > 5) {
+//         throw new Error('images must contain at most 5 items');
+//     }
+//     images.forEach((img, index) => {
+//         if (!isBase64Image(img)) {
+//             throw new Error(`Invalid base64 image format at index ${index}`);
+//         }
+//     });
+// }
 
-function isBase64Image(str: string): boolean {
-    const base64Regex = /^data:image\/(png|jpeg|jpg|gif);base64,/;
-    if (!base64Regex.test(str)) {
-        return false;
-    }
-    try {
-        const base64Data = str.split(',')[1];
-        return btoa(atob(base64Data)) === base64Data;
-    } catch (e) {
-        return false;
-    }
-}
+// function isBase64Image(str: string): boolean {
+//     const base64Regex = /^data:image\/(png|jpeg|jpg|gif);base64,/;
+//     if (!base64Regex.test(str)) {
+//         return false;
+//     }
+//     try {
+//         const base64Data = str.split(',')[1];
+//         return btoa(atob(base64Data)) === base64Data;
+//     } catch (e) {
+//         return false;
+//     }
+// }
 
 function toResponse(blog: any) {
     return blog.toObject ? blog.toObject() : blog;
@@ -47,10 +47,10 @@ export async function createBlog(req: Request, res: Response, next: NextFunction
 		if (!image || !title || !description || !pf) {
 			return void res.status(400).json({ message: 'image, title, description, pf are required' });
 		}
-		validateBase64Image(image);
-		if (images) {
-			validateBase64Images(images);
-		}
+		// validateBase64Image(image);
+		// if (images) {
+		// 	validateBase64Images(images);
+		// }
 		const payload: any = { image, title, description, pf, images: images || [] };
 		if (date) payload.date = new Date(date);
 		const doc = await Blog.create(payload);
@@ -87,7 +87,7 @@ export async function updateBlog(req: Request, res: Response, next: NextFunction
 
 		const update: any = {};
 		if (image !== undefined) {
-			validateBase64Image(image);
+			// validateBase64Image(image);
 			update.image = image;
 		}
 		if (title !== undefined) update.title = title;
@@ -95,7 +95,7 @@ export async function updateBlog(req: Request, res: Response, next: NextFunction
 		if (pf !== undefined) update.pf = pf;
 		if (date !== undefined) update.date = new Date(date);
 		if (images !== undefined) {
-			validateBase64Images(images);
+			// validateBase64Images(images);
 			update.images = images;
 		}
 
